c9f4b63b42fa5fa28a786d2450b247d69f2a4432
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-11-24 14:10:24 +0300

    counter with redux and localStorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,8 +3,21 @@ import {counterReducer} from "./counterReducer";
 const rootReducer = combineReducers({
     counter:counterReducer
 })
-export const store = legacy_createStore(rootReducer)
-export type AppRootStateType = ReturnType<typeof rootReducer>
 
+let preloadedMax;
+let preloadedStart;
+let maxValFromLS = localStorage.getItem('max');
+let startValFromLS = localStorage.getItem('start')
+if(maxValFromLS && startValFromLS){
+    preloadedMax=JSON.parse(maxValFromLS)
+    preloadedStart=JSON.parse(startValFromLS)
+}
+
+export const store = legacy_createStore(rootReducer, preloadedMax, preloadedStart)
+export type AppRootStateType = ReturnType<typeof rootReducer>
+store.subscribe(()=>{
+    localStorage.setItem('start', JSON.stringify(store.getState().counter.StartCounterValue))
+    localStorage.setItem('max', JSON.stringify(store.getState().counter.MAXCounterValue))
+})
 // @ts-ignore
 window.store = store;
